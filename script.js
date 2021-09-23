771e66ffb405bf2bd6196e9bcf4e974c510242ac
Ashutosh Dash <dashashutosh1999@gmail.com>
2021-09-23 14:09:16 +0530

    fix: edited the correct url and other minor fix

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello how are you")
+var inputTxt = document.querySelector("#inputTxt");
+var translateBtn = document.querySelector("#translateBtn");
+var outputTxt = document.querySelector("#outputTxt");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function translateURL(text){
+    return serverURL+"?text="+text;
+}
+
+function showError(error){
+    console.log("error: "+error)
+    alert("error: "+error)
+}
+
+function  clickEvent(){
+    var inputText = inputTxt.value;
+    fetch(translateURL(inputText)).then(response => response.json()).then(json => {
+        outputTxt.value = json.contents.translated
+    }).catch(showError)
+};
+
+translateBtn.addEventListener("click", clickEvent)
